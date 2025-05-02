89fe8bfa7db7a9a24b0a54dd1ce895217b4f91ae
Dirge <fuaddabbad@gmail.com>
2025-05-02 15:13:04 +0700

    fix the Placeholder

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -1,8 +1,8 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+// import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/user-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { Donut  } from '@/components/element/donut';
+import { Donut } from '@/components/element/donut';
 import { Multiple } from '@/components/element/multiple';
 import { ApiDemo } from '@/components/element/plugin';
 
@@ -14,6 +14,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 // import Carousel from '@/components/element/carousel';
+
 import Slider from '@/components/element/carousel';
 
 export default function Dashboard() {
@@ -25,9 +26,9 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
 
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <Donut/>
-                    <ApiDemo/>
-                        <Multiple/>
+                    <Donut />
+                    <ApiDemo />
+                    <Multiple />
                 </div>
                 <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
                     <div className="flex items-center justify-center h-full w-full">
