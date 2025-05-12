aa0153d386c6703742957c0b187f8df85bd852b2
Dirge <fuaddabbad@gmail.com>
2025-05-12 20:35:33 +0700

    add test

---
+++ resources/views/test.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam expedita assumenda corrupti, voluptatum quas quae debitis ratione provident deserunt labore, fuga impedit alias aspernatur rerum libero, itaque repellendus! Quisquam, exercitationem!
+</div>
