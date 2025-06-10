caa6bf56acef877f388f1bdfcc0910a135cabef5
Dirge <fuaddabbad@gmail.com>
2025-06-10 12:28:09 +0700

    update test

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -5,11 +5,21 @@ use App\Models\User;
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
 test('guests are redirected to the login page', function () {
-    $this->get('/dashboard')->assertRedirect('/login');
+    $this->get('borrowings/')->assertRedirect('/login');
 });
 
-test('authenticated users can visit the dashboard', function () {
+test('authenticated users can visit the borrowings page', function () {
     $this->actingAs($user = User::factory()->create());
 
-    $this->get('/dashboard')->assertOk();
+    $this->get('borrowings/')->assertOk();
+});
+
+test('also guests are redirected to the login page', function () {
+    $this->get('collections')->assertRedirect('/login');
+});
+
+test('authenticated users can visit the collection page', function () {
+    $this->actingAs($user = User::factory()->create());
+
+    $this->get('collections')->assertOk();
 });
